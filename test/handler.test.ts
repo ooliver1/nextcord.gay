b3edd1bbc6e2a6bc8602aeed8a8b97e398b22cca
ooliver1 <oliverwilkes2006@icloud.com>
2022-03-13 18:38:41 +0000

    fix: remove pnpm as wrangler doesnt use it

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -9,10 +9,46 @@ describe("handle", () => {
     jest.resetModules();
   });
 
-  test("handle GET", async () => {
-    const result = await handleRequest(new Request("/", { method: "GET" }));
+  test("handle bot", async () => {
+    const result = await handleRequest(
+      new Request("/", {
+        method: "GET",
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)",
+        },
+      })
+    );
     expect(result.status).toEqual(200);
     const text = await result.text();
-    expect(text).toEqual("request method: GET");
+    expect(text).toContain("Nextcord");
+  });
+
+  test("handle user", async () => {
+    const result = await handleRequest(
+      new Request("/", {
+        method: "GET",
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+            "(KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.70",
+        },
+      })
+    );
+    expect(result.status).toEqual(301);
+    expect(result.headers.get("location")).toEqual(
+      "https://youtube.com/watch?v=dQw4w9WgXcQ"
+    );
+  });
+
+  test("handle no user-agent", async () => {
+    const result = await handleRequest(
+      new Request("/", {
+        method: "GET",
+      })
+    );
+    expect(result.status).toEqual(200);
+    const text = await result.text();
+    expect(text).toEqual("No user-agent, impressive!");
   });
 });
