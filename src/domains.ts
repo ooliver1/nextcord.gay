c5a135287cc5bef35aad2d655eebab2c18eae16c
teaishealthy <teaishealthy@protonmail.com>
2022-03-28 18:37:19 +0200

    feat: add floppy rickroll

--- src/domains.ts
+++ src/domains.ts
@@ -1,6 +1,7 @@
 const domains: Domains = {
   "https://www.youtube.com/watch?v=dQw4w9WgXcQ": "rickrolled another user, h",
   "https://www.youtube.com/watch?v=L7l8j9raHVA": "switchrolled another user, h",
+  "https://www.youtube.com/watch?v=63qtYi1nwcs": "flopped another user, h",
 };
 
 export default domains;
