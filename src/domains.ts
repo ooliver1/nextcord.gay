2d4636f9e30a8bd8cc496667109699dfb59602f7
ooliver1 <oliverwilkes2006@icloud.com>
2022-04-02 14:51:03 +0100

    feat: more domains yay

--- src/domains.ts
+++ src/domains.ts
@@ -2,6 +2,9 @@ const domains: Domains = {
   "https://www.youtube.com/watch?v=dQw4w9WgXcQ": "rickrolled another user, h",
   "https://www.youtube.com/watch?v=L7l8j9raHVA": "switchrolled another user, h",
   "https://www.youtube.com/watch?v=63qtYi1nwcs": "flopped another user, h",
+  "https://www.youtube.com/watch?v=zKJ6xGVzrnI": "madelined another user, h",
+  "https://www.youtube.com/watch?v=47ibFGy-w18": "voicecracked another user, h",
+  "https://www.youtube.com/watch?v=jzwMjOl8Iyo": "toothbrushed another user, h",
 };
 
 export default domains;
